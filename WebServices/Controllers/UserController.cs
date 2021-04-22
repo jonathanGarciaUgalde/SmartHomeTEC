1a87fe3212ea87f4a77a5a851a87f9096bf037f3
Jonathan García Ugalde <jonagar@hotmail.com>
2021-04-22 00:52:59 -0600

    v1

--- WebServices/Controllers/UserController.cs
+++ WebServices/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using WebServices.Models;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebServices.Controllers
@@ -12,36 +13,51 @@ namespace WebServices.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<ValuesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/<ValuesController>/5
+        /*
+        * Método que se comunica mediante el protocolo http para validar si el usuario que inicia sesión está registrado.
+        */
+        [HttpGet("{Username}/{Password}")]
+        public async Task<IActionResult> ValidUser(string Username, string Password)
+        {
+            return Ok();
+        }
+        /*
+        * Método que se comunica mediante el protocolo http y retorna todos los usuarios registrados.
+        */
+        [HttpGet("All/Users")]
+        public async Task<IActionResult> GetallAsync()
+        {
+            return Ok();
+        }
+        /*
+        * Método que se comunica mediante el protocolo http  y  este retorna el usuario del id que se consulta 
+        * 
+        */
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetClienteAsync(int id)
         {
-            return "value";
+         
+            return Ok();
         }
-
-        // POST api/<ValuesController>
+        /*
+        * Método que se comunica mediante el protocolo http para insertar nuevos clientes en el app.
+        */
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> AddClienteAsync(User newUser)
         {
+          
+            return CreatedAtRoute("default", new { id = newUser.Correo}, newUser);
         }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
+      
+        /*
+        * Método que se comunica mediante el protocolo http para eliminar el usuario que se indica en la página web
+        */
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteClienteAsync(int id)
         {
+           
+            return Ok("toDelete");
         }
     }
 }
